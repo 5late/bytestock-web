55e7959fd5ed4478d4837e8414ad8528fd60a151
5late <christianrfernandes5@gmail.com>
2023-02-26 02:00:02 -0500

    added cross compatibility between web and core

--- Home.py
+++ Home.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
+import sys
+sys.path.append('bytestock-core')
+
+import data
 
 st.set_page_config(initial_sidebar_state='collapsed')
 
@@ -11,3 +15,7 @@ st.image(logo)
 stock = st.text_input(label='See data over a specified time period.', placeholder='Enter a stock ticker: (Ex. AAPL)', max_chars=5)
 
 st.write('You selected: $' + stock)
+
+open_days, daily_open, daily_close, daily_high, daily_low = data.getOCHLData(stock, 3)
+
+st.write('Open: ', str(daily_open))
