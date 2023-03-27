85f95fb69a608bad7498a04e4cd63a5291d4e90e
Slate <46351560+5late@users.noreply.github.com>
2023-03-27 14:11:26 +0000

    updated label

--- Home.py
+++ Home.py
@@ -13,7 +13,7 @@ st.set_page_config(initial_sidebar_state='collapsed')
 logo = Image.open('pics/bytestock-logo.png')
 st.image(logo)
 
-stock = st.text_input(label='See data over a specified time period.', placeholder='Enter a stock ticker: (Ex. AAPL)', max_chars=5)
+stock = st.text_input('See data over a specified time period.', placeholder='Enter a stock ticker: (Ex. AAPL)', max_chars=5)
 
 st.write('You selected: $' + stock)
 
