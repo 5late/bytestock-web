0d8519d66b180983877de1f2cb3e3057a662b978
Nolawi Teklehaimanot <nolawiteklehaimanot@gmail.com>
2023-04-01 15:15:39 -0400

    change

--- pages/About.py
+++ pages/About.py
@@ -1,7 +1,7 @@
-import streamlit as st
-
-with open('./pages/about.md') as file:
-    lines = file.readlines()
-
-    for line in lines:
-        st.write(line)
+import streamlit as st 
+ 
+with open('./pages/about.md') as file: 
+    lines = file.readlines() 
+ 
+    for line in lines: 
+        st.write(line) 
--- pages/Live_Stocks.py
+++ pages/Live_Stocks.py
@@ -1,32 +1,32 @@
-import streamlit as st
-import sys
-sys.path.append('../bytestock-core')
+import streamlit as st 
+import sys 
+sys.path.append('../bytestock-core') 
+ 
+import data 
 
-import data
+st.set_page_config(layout="wide") 
 
-st.set_page_config(layout="wide")
-
-col1, col2, col3, col4, col5, col6 = st.columns(6)
-st.text("")
-st.text("")
-st.text("")
-st.text("")
-st.text("")
+col1, col2, col3, col4, col5, col6 = st.columns(6) 
+st.text("") 
+st.text("") 
+st.text("") 
+st.text("") 
+st.text("") 
 col7, col8, col9, col10, col11, col12 = st.columns(6)
-st.text("")
-st.text("")
-st.text("")
-st.text("")
-st.text("")
+st.text("") 
+st.text("") 
+st.text("") 
+st.text("") 
+st.text("") 
 col13, col14, col15, col16, col17, col18 = st.columns(6)
 cols = [col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14, col15, col16, col17, col18]
-
-
+ 
+ 
 info = []
-
+ 
 with open('./pages/stocks.txt', 'r') as f:
     lines = f.readlines()
-
+ 
 for i in range(18):
     stock = lines[i].rstrip()
 
